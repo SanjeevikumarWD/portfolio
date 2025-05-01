0831509764d142a3d84d7be27a55ea0b7aa33f40
SANJEEVIKUMAR <126496788+SanjeevikumarWD@users.noreply.github.com>
2025-05-01 19:53:06 +0530

    Update Skills.jsx
    skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,20 +1,40 @@
 import React from "react";
 import { FaReact, FaNodeJs, FaGit, FaHtml5, FaCss3Alt } from "react-icons/fa";
-import { SiMongodb, SiJavascript, SiTailwindcss, SiTypescript } from "react-icons/si";
+import {
+  SiMongodb,
+  SiJavascript,
+  SiTailwindcss,
+  SiMysql,
+  SiVueDotJs,
+  SiQuasar,
+  SiPython,
+} from "react-icons/si";
 import { MdOutlineWeb } from "react-icons/md";
 import { BsFillPhoneFill } from "react-icons/bs";
 
 const skills = [
-  { name: "React", icon: <FaReact /> },
-  { name: "Node.js", icon: <FaNodeJs /> },
-  { name: "MongoDB", icon: <SiMongodb /> },
-  { name: "Express", icon: <FaNodeJs /> },
-  { name: "JavaScript", icon: <SiJavascript /> },
-  { name: "TypeScript", icon: <SiTypescript /> },
+  // Frontend Technologies
   { name: "HTML", icon: <FaHtml5 /> },
   { name: "CSS", icon: <FaCss3Alt /> },
+  { name: "JavaScript", icon: <SiJavascript /> },
+  { name: "React", icon: <FaReact /> },
+  { name: "Vue.js", icon: <SiVueDotJs /> },
+  { name: "Quasar", icon: <SiQuasar /> },
   { name: "Tailwind CSS", icon: <SiTailwindcss /> },
+
+  // Backend Technologies
+  { name: "Node.js", icon: <FaNodeJs /> },
+  { name: "Express", icon: <FaNodeJs /> },
+  { name: "Python", icon: <SiPython /> },
+
+  // Databases
+  { name: "MongoDB", icon: <SiMongodb /> },
+  { name: "MySQL", icon: <SiMysql /> },
+
+  // Tools
   { name: "Git", icon: <FaGit /> },
+
+  // Others
   { name: "Web Design", icon: <MdOutlineWeb /> },
   { name: "Responsive Design", icon: <BsFillPhoneFill /> },
 ];
@@ -22,12 +42,14 @@ const skills = [
 const Skills = () => {
   return (
     <div className="p-6">
-      <h2 className="text-2xl md:text-3xl lg:text-5xl italic  font-rosarivo cursor-default my-2">Tech Stack</h2>
+      <h2 className="text-2xl md:text-3xl lg:text-5xl italic font-rosarivo cursor-default my-2">
+        Tech Stack
+      </h2>
       <div className="flex flex-wrap gap-6">
         {skills.map((skill, index) => (
           <div
-            key={skill.name}
-            className={`flex items-center gap-2 p-3 shadow-md rounded-lg text-lg bg-text text-bg cursor-default`}
+            key={index}
+            className="flex items-center gap-2 p-3 shadow-md rounded-lg text-lg bg-text text-bg cursor-default"
           >
             <div className="text-2xl">{skill.icon}</div>
             <span>{skill.name}</span>
